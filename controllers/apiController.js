1fd6e2abf3acee40d7fe3a514567990af09b8154
dev <dev>
2016-09-03 13:37:55 +0200

    Changed the api/person responce

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -5,14 +5,14 @@ var ref =require('../firebase/firebase');
 var jsonParser = bodyParser.json();
 
 module.exports = function(app) {
-	
+
 	app.get('/api/person/:id', function(req, res,next) {
 	// get that data from database
 		res.json({ firstname: 'John', lastname: 'Doe' });
 	});
 
 	app.post('/api/person',jsonParser, function(req, res,next) {
-		res.send('Thank you for the JSON data!');
+		res.send('Thank you for the JSON data! firstnamev: '+req.body.firstname+' lastname : '+req.body.lastname);
 		console.log(req.body.firstname);
 		console.log(req.body.lastname);
 		ref.once("value", function(snapshot) {
